8b43c9d01ff8e1147a5976f791f1b1ced9cee06b
Badax39 <bad211@wp.pl>
2023-04-30 23:24:11 +0200

    Zadanie domowe dzień 3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("It's alive!");
+﻿string name;
+char sex;
+int age;
+
+name = "Ewa";
+sex = 'm';
+age = 100;
+
+if (sex == 'k' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30");
+}
+else
+{
+    if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else
+    {
+        if (age < 18 && sex == 'm')
+        {
+            Console.WriteLine("Niepełnoletni mężczyzna");
+        }
+        else
+        {
+            Console.WriteLine("Żaden z warunków nie został spełniony");
+        }
+    }
+}
+
+
